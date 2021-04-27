0a4a84b293c3508c6834f38f3989abadc1ada0aa
Kian <kian.p.m.007@gmail.com>
2021-04-26 23:36:57 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var countDownDate = new Date("May 27, 2021, 15:45:00").getTime();
+
+var x = setInterval(updateTime, 1000)
+
+function updateTime() {
+    var now = new Date().getTime();
+
+    var timeBetween = countDownDate - now;
+
+    var days = Math.floor(timeBetween / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeBetween % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeBetween % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeBetween % (1000 * 60)) / 1000);
+  
+    document.getElementById("timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+}
