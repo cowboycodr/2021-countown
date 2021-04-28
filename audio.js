a3a4a807fa8f79a3e3b821de4b82809de6587eab
Kian <kian.p.m.007@gmail.com>
2021-04-28 18:26:49 -0500

    update soundboard

--- audio.js
+++ audio.js
@@ -43,6 +43,8 @@ function clickBody() {
 document.onkeydown = function(e) {
     var key = e.key;
 
+    console.log(key)
+
     if (key == 1) {
         var fortniteAudio = document.getElementById("fortnite-audio");
 
@@ -72,5 +74,30 @@ document.onkeydown = function(e) {
         pauseAll();
     } else if (key == "Control") {
         playAll();
+    }
+
+
+    if (key == " ") {
+        var canvasFeature = 
+            "location=no,toolbar=no,menubar=no,scrollbar=yes,resizable=yes,width="+screen.availWidth+",height="+screen.availHeight;
+
+        var canvas = window.open(
+            "https://smsd.instructure.com",
+            "_blank",
+            canvasFeature
+        );
+
+        console.log(canvasFeature);
+    } else if (key == "Escape") {
+        var popupFeature = 
+        "location=no,toolbar=no,menubar=no,scrollbar=yes,resizable=yes,width="+screen.availWidth+",height="+screen.availHeight;
+
+        var popupCountdown = window.open(
+            "https://cowboycodr.github.io",
+            "blank_",
+            popupFeature
+        )
+
+        close();
     }
 } 
