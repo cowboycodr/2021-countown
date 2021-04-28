40ed7807ceb3466cfbdbdb1bfcfc2eee6ba09485
Kian McKenna <59800761+cowboycodr@users.noreply.github.com>
2021-04-28 11:39:47 -0500

    audio loop functionality

--- audio.js
+++ audio.js
@@ -1,6 +1,6 @@
 function clickBody() {
     var audio = document.getElementById("audio");
-    if (audio.duration > 0 && !audio.paused) {
+    if (!audio.paused) {
         audio.pause();
     } else {
         audio.play();
