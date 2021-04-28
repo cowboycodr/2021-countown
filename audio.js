83e96102ca944992172fe65fbb54311e6990b232
Kian <kian.p.m.007@gmail.com>
2021-04-28 17:39:35 -0500

    soundboard update

--- audio.js
+++ audio.js
@@ -1,8 +1,78 @@
+function pauseAll() {
+    var minecraftAudio = document.getElementById("minecraft-audio");
+    var fortniteAudio = document.getElementById("fortnite-audio");
+    var letsgoAudio = document.getElementById("lets-go-audio");
+    var yeayeaAudio = document.getElementById("yea-yea-audio");
+    var windowserrorAudio = document.getElementById("windows-error-audio");
+    var aliaintroAudio = document.getElementById("alia-intro-audio");
+
+    minecraftAudio.pause();
+    fortniteAudio.pause();
+    letsgoAudio.pause();
+    yeayeaAudio.pause();
+    windowserrorAudio.pause();
+    aliaintroAudio.pause();
+}
+
+function playAll() {
+    var minecraftAudio = document.getElementById("minecraft-audio");
+    var fortniteAudio = document.getElementById("fortnite-audio");
+    var letsgoAudio = document.getElementById("lets-go-audio");
+    var yeayeaAudio = document.getElementById("yea-yea-audio");
+    var windowserrorAudio = document.getElementById("windows-error-audio");
+    var aliaintroAudio = document.getElementById("alia-intro-audio");
+
+    minecraftAudio.play();
+    fortniteAudio.play();
+    letsgoAudio.play();
+    yeayeaAudio.play();
+    windowserrorAudio.play();
+    aliaintroAudio.play();
+}
+
 function clickBody() {
-    var audio = document.getElementById("audio");
-    if (!audio.paused) {
-        audio.pause();
+    var minecraftAudio = document.getElementById("minecraft-audio");
+
+    if (!minecraftAudio.paused) {
+        minecraftAudio.pause();
     } else {
-        audio.play();
+        minecraftAudio.play();
     }
 }
+
+document.onkeydown = function(e) {
+    var key = e.key;
+
+    console.log(key);
+
+    if (key == 1) {
+        var fortniteAudio = document.getElementById("fortnite-audio");
+
+        pauseAll();
+        fortniteAudio.play();
+    } else if (key == 2) {
+        var letsgoAudio = document.getElementById("lets-go-audio");
+
+        pauseAll();
+        letsgoAudio.play();
+    } else if (key == 3) {
+        var yeayeaAudio = document.getElementById("yea-yea-audio");
+
+        pauseAll();
+        yeayeaAudio.play();
+    } else if (key == 4) {
+        var windowserrorAudio = document.getElementById("windows-error-audio");
+
+        pauseAll();
+        windowserrorAudio.play();
+    } else if (key == 5) {
+        var aliaintroAudio = document.getElementById("alia-intro-audio");
+
+        pauseAll();
+        aliaintroAudio.play();
+    } else if (key == "`") {
+        pauseAll();
+    } else if (key == "Control") {
+        playAll();
+    }
+} 
